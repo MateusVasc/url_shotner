3d09805d9db42a5ff76c234530b2db1ddddda9f3
MateusVasc <mvaraujo2004@gmail.com>
2026-01-06 20:42:48 -0300

    fix: missing param on query

--- src/main/java/com/matt/url_shotner/repositories/LinkRepository.java
+++ src/main/java/com/matt/url_shotner/repositories/LinkRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface LinkRepository extends JpaRepository<Link, String> {
-    @Query("SELECT l.originalUrl FROM Link l WHERE l.shortUrl = :")
+    @Query("SELECT l.originalUrl FROM Link l WHERE l.shortUrl = :shortUrl")
     String findLongUrlByShortUrl(String shortUrl);
 }
