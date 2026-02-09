c2750c0ec7a513c1ea3b50d0e2c2c2f31f663a60
MateusVasc <mvaraujo2004@gmail.com>
2026-02-09 18:15:49 -0300

    feat: adds jwt custom filter

---
+++ src/main/java/com/matt/url_shotner/infra/security/JwtAuthFilter.java
@@ -0,0 +1,51 @@
+package com.matt.url_shotner.infra.security;
+
+import com.matt.url_shotner.entities.User;
+import com.matt.url_shotner.infra.jwt.JwtUtils;
+import com.matt.url_shotner.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtUtils jwtUtils;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = request.getHeader("Authorization");
+
+        if (accessToken == null || !accessToken.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        accessToken = accessToken.replace("Bearer ", "");
+
+        String email = this.jwtUtils.validateAccessToken(accessToken);
+
+        Optional<User> optUser = this.userRepository.findByEmail(email);
+
+        if (optUser.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("User not found for given token");
+            return;
+        }
+
+        CustomUserDetails userDetails = CustomUserDetails.build(optUser.get());
+        var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+}
