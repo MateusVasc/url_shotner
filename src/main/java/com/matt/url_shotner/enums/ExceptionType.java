fa9e037f5a02dfdb71dac0140e3cc8b63922c9dd
MateusVasc <mvaraujo2004@gmail.com>
2026-02-09 18:13:56 -0300

    feat: adds new exceptions

--- src/main/java/com/matt/url_shotner/enums/ExceptionType.java
+++ src/main/java/com/matt/url_shotner/enums/ExceptionType.java
@@ -5,7 +5,11 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionType {
     LINK_NOT_FOUND_EXCEPTION("There is no short URL for this given link", HttpStatus.NOT_FOUND),
     USER_NOT_FOUND_EXCEPTION("User not found", HttpStatus.NOT_FOUND),
-    USER_ALREADY_EXISTS("An user with this e-mail already exists", HttpStatus.CONFLICT);
+    USER_ALREADY_EXISTS("An user with this e-mail already exists", HttpStatus.CONFLICT),
+    INCORRECT_CREDENTIALS("Incorrect credentials", HttpStatus.UNAUTHORIZED),
+    FAILED_TO_RETRIEVE_EXPIRATION_TIME_FROM_TOKEN("Failed to get expiration time from token", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_TOKEN("Invalid token", HttpStatus.INTERNAL_SERVER_ERROR),
+    FAILED_TO_CREATE_TOKEN("Failed to create token", HttpStatus.INTERNAL_SERVER_ERROR);
 
     public final String message;
     public final HttpStatus status;
